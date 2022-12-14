d7f1bfd864785d5214a3cf5df788903e0376f485
GtDavide01 <davidefiorini01@gmail.com>
2022-12-14 13:39:39 +0100

    aggiunte variabili con compact

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $title = "Sono la prima pagina in laravel";
+    return view('home', compact("title"));
 });
